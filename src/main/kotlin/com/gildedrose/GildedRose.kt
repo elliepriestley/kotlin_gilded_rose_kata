85c85b7f109bcfd6e46e5a475011a1ef469956fe
elliepriestley <ellie.priestley@gmail.com>
2024-02-01 16:55:02 +0000

    Merged some if statements

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,26 +8,20 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         items.forEach { item ->
             if (item.name != BRIE && item.name != BACKSTAGE) {
-                if (item.quality > 0) {
-                    if (item.name != SULFURAS) {
-                        item.quality -= 1
-                    }
+                if (item.quality > 0 && item.name != SULFURAS) {
+                    item.quality -= 1
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality += 1
 
                     if (item.name == BACKSTAGE) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality += 1
                         }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality += 1
-                            }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality += 1
                         }
                     }
                 }
@@ -40,18 +34,14 @@ class GildedRose(var items: List<Item>) {
             if (item.sellIn < 0) {
                 if (item.name != BRIE) {
                     if (item.name != BACKSTAGE) {
-                        if (item.quality > 0) {
-                            if (item.name != SULFURAS) {
-                                item.quality -= 1
-                            }
+                        if (item.quality > 0 && item.name != SULFURAS) {
+                            item.quality -= 1
                         }
                     } else {
                         item.quality = 0
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality += 1
-                    }
+                } else if (item.quality < 50) {
+                    item.quality += 1
                 }
             }
         }
