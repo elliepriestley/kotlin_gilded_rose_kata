0c5aef0b9a1167fca28b1e5061e810a406ebb370
elliepriestley <ellie.priestley@gmail.com>
2024-02-01 17:23:44 +0000

    Extracted quality logic into functions and moved to when statement

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,51 +1,67 @@
 package com.gildedrose
 
 class GildedRose(var items: List<Item>) {
-    private val BRIE = "Aged Brie"
-    private val SULFURAS = "Sulfuras, Hand of Ragnaros"
-    private val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+
+    private val brie = "Aged Brie"
+    private val sulfuras = "Sulfuras, Hand of Ragnaros"
+    private val backstage = "Backstage passes to a TAFKAL80ETC concert"
+    private val specialItems = listOf(brie, sulfuras, backstage)
+
 
     fun updateQuality() {
         items.forEach { item ->
-            if (item.name != BRIE && item.name != BACKSTAGE) {
-                if (item.quality > 0 && item.name != SULFURAS) {
-                    item.quality -= 1
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality += 1
 
-                    if (item.name == BACKSTAGE) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality += 1
-                        }
-
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality += 1
-                        }
-                    }
-                }
+            when (item.name) {
+                brie -> increaseQuality(item)
+                backstage -> increaseQualityForBackstage(item)
+                sulfuras -> increaseQuality(item)
+                else -> decreaseQuality(item)
             }
 
-            if (item.name != SULFURAS) {
+
+            //SELL IN
+
+            if (item.name != sulfuras) {
                 item.sellIn -= 1
             }
 
             if (item.sellIn < 0) {
-                if (item.name != BRIE) {
-                    if (item.name != BACKSTAGE) {
-                        if (item.quality > 0 && item.name != SULFURAS) {
-                            item.quality -= 1
+                if (item.name != brie) {
+                    if (item.name != backstage) {
+                        if (item.quality > 0 && item.name != sulfuras) {
+                            decreaseQuality(item)
                         }
                     } else {
                         item.quality = 0
                     }
                 } else if (item.quality < 50) {
-                    item.quality += 1
+                    increaseQuality(item)
                 }
             }
         }
     }
 
+    private fun decreaseQuality(item: Item) {
+        if (item.quality > 0) {
+            item.quality -= 1
+        }
+    }
+
+    private fun increaseQualityForBackstage(item: Item) {
+            increaseQuality(item)
+            if (item.sellIn < 11) {
+                increaseQuality(item)
+            }
+            if (item.sellIn < 6) {
+                increaseQuality(item)
+            }
+    }
+
+    private fun increaseQuality(item: Item) {
+        if (item.quality < 50) {
+            item.quality += 1
+        }
+
+}
 }
 
