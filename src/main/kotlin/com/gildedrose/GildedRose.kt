926e1028ea3276c354a004490fb9978260f86d05
elliepriestley <ellie.priestley@gmail.com>
2024-02-01 16:47:37 +0000

    Created constants for items

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,53 +1,56 @@
 package com.gildedrose
 
 class GildedRose(var items: List<Item>) {
+    private val BRIE = "Aged Brie"
+    private val SULFURAS = "Sulfuras, Hand of Ragnaros"
+    private val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
 
     fun updateQuality() {
         items.forEach { item ->
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.name != BRIE && item.name != BACKSTAGE) {
                 if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
+                    if (item.name != SULFURAS) {
+                        item.quality -= 1
                     }
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.name == BACKSTAGE) {
                         if (item.sellIn < 11) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
 
                         if (item.sellIn < 6) {
                             if (item.quality < 50) {
-                                item.quality = item.quality + 1
+                                item.quality += 1
                             }
                         }
                     }
                 }
             }
 
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
+            if (item.name != SULFURAS) {
+                item.sellIn -= 1
             }
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.name != BRIE) {
+                    if (item.name != BACKSTAGE) {
                         if (item.quality > 0) {
-                            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                                item.quality = item.quality - 1
+                            if (item.name != SULFURAS) {
+                                item.quality -= 1
                             }
                         }
                     } else {
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                     }
                 } else {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1
+                        item.quality += 1
                     }
                 }
             }
