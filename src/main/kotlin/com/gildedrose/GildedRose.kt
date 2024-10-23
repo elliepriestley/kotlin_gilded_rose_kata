4fbf7d55728ff8ef580f7098ee3c4001a681f0a7
Sarah Clements <sarah.clements@sedex.com>
2024-10-23 16:17:14 +0100

    Extract logic into method

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,11 +5,7 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
+                updateQualityOfSulfuras(i)
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
@@ -37,11 +33,7 @@ class GildedRose(var items: List<Item>) {
             if (items[i].sellIn < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
+                        updateQualityOfSulfuras(i)
                     } else {
                         items[i].quality = items[i].quality - items[i].quality
                     }
@@ -50,6 +42,14 @@ class GildedRose(var items: List<Item>) {
                         items[i].quality = items[i].quality + 1
                     }
                 }
+            }
+        }
+    }
+
+    private fun updateQualityOfSulfuras(i: Int) {
+        if (items[i].quality > 0) {
+            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                items[i].quality = items[i].quality - 1
             }
         }
     }
