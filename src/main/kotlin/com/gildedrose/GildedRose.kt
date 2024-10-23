887dfe56d765d84f2ce57190102f728b06a8f9f1
Sarah Clements <sarah.clements@sedex.com>
2024-10-23 16:19:22 +0100

    Extract logic for quality less than 50

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,23 +7,7 @@ class GildedRose(var items: List<Item>) {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 updateQualityOfSulfuras(i)
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+                updateQualityIfLessThan50(i)
             }
 
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
@@ -38,6 +22,26 @@ class GildedRose(var items: List<Item>) {
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updateQualityIfLessThan50(i: Int) {
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
+
+            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].sellIn < 11) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                }
+
+                if (items[i].sellIn < 6) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
                     }
