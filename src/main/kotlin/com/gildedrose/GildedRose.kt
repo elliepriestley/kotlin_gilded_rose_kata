98ece6e2092f981e926859615864f4663190be04
elliepriestley <ellie.priestley@gmail.com>
2024-10-23 15:57:32 +0100

    Reset kata

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,66 +2,57 @@ package com.gildedrose
 
 class GildedRose(var items: List<Item>) {
 
-    private val brie = "Aged Brie"
-    private val sulfuras = "Sulfuras, Hand of Ragnaros"
-    private val backstage = "Backstage passes to a TAFKAL80ETC concert"
-    private val specialItems = listOf(brie, sulfuras, backstage)
-
-
     fun updateQuality() {
-        items.forEach { item ->
+        for (i in items.indices) {
+            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].quality > 0) {
+                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        items[i].quality = items[i].quality - 0
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1
 
-            when (item.name) {
-                brie -> increaseQuality(item)
-                backstage -> increaseQualityForBackstage(item)
-                sulfuras -> increaseQuality(item)
-                else -> decreaseQuality(item)
+                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1
+                            }
+                        }
+                    }
+                }
             }
 
-            //SELL IN
-            if (item.name != sulfuras) {
-                increaseSellIn(item)
+            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                items[i].sellIn = items[i].sellIn - 1
             }
 
-
-
-            if (item.sellIn < 0) {
-
-                when (item.name) {
-                    brie -> increaseQuality(item)
-                    backstage -> item.quality = 0
-                    sulfuras -> item
-                    else -> decreaseQuality(item)
+            if (items[i].sellIn < 0) {
+                if (items[i].name != "Aged Brie") {
+                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (items[i].quality > 0) {
+                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                items[i].quality = items[i].quality - 1
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
                 }
             }
         }
     }
 
-    private fun increaseSellIn(item: Item) {
-        item.sellIn -= 1
-    }
-
-    private fun decreaseQuality(item: Item) {
-        if (item.quality > 0) {
-            item.quality -= 1
-        }
-    }
-
-    private fun increaseQualityForBackstage(item: Item) {
-            increaseQuality(item)
-            if (item.sellIn < 11) {
-                increaseQuality(item)
-            }
-            if (item.sellIn < 6) {
-                increaseQuality(item)
-            }
-    }
-
-    private fun increaseQuality(item: Item) {
-        if (item.quality < 50) {
-            item.quality += 1
-        }
-
-}
 }
 
